4255a57a7f5e36244e1ea2014047c5e424d8c285
Mary Ojo <maryojo3@gmail.com>
2023-02-12 16:02:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useLayoutEffect, useRef } from "react";
+import "./index.css";
 
-function App() {
+gsap.registerPlugin(ScrollTrigger);
+
+export default function App() {
+  const component = useRef();
+  const slider = useRef();
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      let panels = gsap.utils.toArray(".panel");
+      gsap.to(panels, {
+        xPercent: -100 * (panels.length - 1),
+        ease: "none",
+        scrollTrigger: {
+          trigger: slider.current,
+          pin: true,
+          scrub: 1,
+          snap: 1 / (panels.length - 1),
+          end: () => "+=" + slider.current.offsetWidth
+        }
+      });
+    }, component);
+    return () => ctx.revert();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={component}>
+      <div className="firstContainer">
+        <h1>Testing horizontal scrolling w/ three sections</h1>
+        <h2>First Container</h2>
+      </div>
+      <div ref={slider} className="container">
+        <div className="description panel blue">
+          <div>
+            SCROLL DOWN
+            <div className="scroll-down">
+              <div className="arrow"></div>
+            </div>
+          </div>
+        </div>
+        <div className="panel red">ONE</div>
+        <div className="panel orange">TWO</div>
+        <div className="panel purple">THREE</div>
+      </div>
+      <div className="lastContainer">Last Container</div>
     </div>
   );
 }
-
-export default App;
